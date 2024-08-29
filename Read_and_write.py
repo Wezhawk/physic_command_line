03e84a97907debd0293385ae6e6c0d638a9556d9
Wezhawk <wes.carrington@gmail.com>
2024-08-29 14:36:17 -0500

    added delete command

--- Read_and_write.py
+++ Read_and_write.py
@@ -1,5 +1,9 @@
+#This program is a basic command line interface that can read and write files
+
+import os
+
 print("Welcome to the listing command line\n\n")
-print("Compiled for Windows using Pyinstaller, and installation program created with Actual Installer\n")
+print("Compiled for Windows using Pyinstaller, and installation program was created with Actual Installer\n")
 print("Copyright 2024 Everett Carrington\n\n\n\n")
 username = input("Please enter your username: ")
 if len(username) == 0:
@@ -29,6 +33,7 @@ def command_handler(prompt):
     if "create" in prompt:
         if prompt[7:] == "-h" or prompt[7:] == "--help":
             print("\nCreate Command Help:\n\nEnter the create command followed by a filename argument to make a new writable file\n\nExample: 'create myfile'\n\nNote: A .txt file extension will automatically be added to the filename\n\n")
+            return        
         if prompt[6:] == "":
             print("Please specify filename!")
             return
@@ -36,6 +41,30 @@ def command_handler(prompt):
             print("Command not recognized!")
             return
         filename = prompt[7:] + ".txt"
+        if os.path.exists(filename):
+            print("File already exists!")
+            return
+        f = open(filename, "x")
+        print("Successfully created " + filename)
+        return
+    if "delete" in prompt:
+        if prompt[7:] == "-h" or prompt[7:] == "--help":
+            print("\nDelete Command Help:\n\nEnter the delete command followed by a filename argument to delete a file\n\nExample: 'delete myfile'\n\nNote: A .txt file extension will automatically be added to the filename\n\n")
+            return
+        if prompt[6:] == "":
+            print("Please specify filename!")
+            return
+        if prompt[6] != " ":
+            print("Command not recognized!")
+            return
+        filename = prompt[7:] + ".txt"
+        if os.path.exists(filename):
+            os.remove(filename)
+            print(filename + " was deleted")
+            return
+        else:
+            print("File does not exist!")
+            return
     if prompt == "exit":
         print("Script Terminating!!!\n")
         exit()
