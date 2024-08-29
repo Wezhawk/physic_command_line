240fff740c6f7752fa9197eb23d056ef43febea8
Wezhawk <wes.carrington@gmail.com>
2024-08-29 13:13:44 -0500

    Added start of code(up to username check) in Read_and_write.py

---
+++ Read_and_write.py
@@ -0,0 +1,17 @@
+import time.sleep as sleep
+
+print("Welcome to the listing command line\n\n")
+print("Compiled for Windows using Pyinstaller, and installation program created with actual installer\n\n\n")
+print("Copyright 2024 Everett Carrington\n\n\n\n")
+username = input("Please enter your username: ")
+if len(username) == 0:
+    username = input("Please try again: ")
+    if len(username) == 0:
+        username = input("Please try again: ")
+        if len(username) == 0:
+            username = input("Please try again: ")
+            if len(username) == 0:
+                print("Script Terminating!!!")
+                sleep(1)
+                exit
+
